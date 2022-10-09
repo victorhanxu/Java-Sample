fb18bcb9c4907621a9cede1428fb6ae8c3bb3ddf
Victor Han <45407000+victorhanxu@users.noreply.github.com>
2022-10-09 18:52:27 +0000

    object mapper init

--- objectMapperTest/src/main/java/org/example/Main.java
+++ objectMapperTest/src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonMappingException, JsonProcessingException {
         System.out.println("Hello world!");
+
+        String jsonString = "{\"name\":\"Hyl\", \"age\":20}";
+
+        ObjectMapper mapper=new ObjectMapper();
+
+        Student student=mapper.readValue(jsonString, Student.class);
+
+        System.out.println(student);
+
+
+
+    }
+
+    static class Student {
+        String name;
+        String age;
+
+
+        public String toString() {
+            return "name: "+name+","+"age: "+age;
+        }
     }
 }
